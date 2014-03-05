c9a929c3b28e398d4edf1f3cb9ca1bc00c39bc2c
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-04 20:31:19 -0600

    added data.js for temporary data and views folder

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var engines = require('consolidate');
 var app = express();
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
-app.use(express.methodOverride());
-app.listen(3000);
+var data = require('./data')
 
-app.get('/', function(req, res){
-  res.render('index');
+app.set("views", __dirname + "/views");
+app.engine(".handlebars", engines.handlebars);
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/assets'));
+app.use(express.methodOverride());
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.render("index", data); 
 });
+
+app.listen(3000);
 console.log('Listening on port 3000');
