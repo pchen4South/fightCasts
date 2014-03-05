b87bd60f39e4fdb8f4becf5eab177e8f021f6ace
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-05 15:48:50 -0600

    refactored api

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
-var async = require('async')
-
+var async = require('async');
+var _ = require('lodash');
+var partial = _.partial;
 var player = require('./models/playerModel');
 var character = require('./models/characterModel');
 var game = require('./models/gameModel');
@@ -8,92 +9,63 @@ var event = require('./models/eventModel');
 var video = require('./models/videoModel');
 var channel = require('./models/channelModel');
 var team = require('./models/teamModel');
+var fighter = require('./models/fighterModel');
 
+var create = function (modelType, data, cb) {
+  return modelType.create(data, cb);
+};
 
-var createPlayer = function(req,res){
-  player['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
+//create
+var createPlayer = partial(create, player.model);
+var createCharacter = partial(create, character.model);
+var createGame = partial(create, game.model);
+var createVideo = partial(create, video.model);
+var createCaster = partial(create, caster.model);
+var createEvent = partial(create, event.model);
+var createChannel = partial(create, channel.model);
+var createTeam = partial(create, team.model);
+
+var createFighter = function (data, cb) {
+  var formatted = {
+    _player: data.player,
+    _characters: data.characters 
+  };
+
+  return fighter.model.create(formatted, cb);
+};
+
+var get = function (modelType, id, cb) {
+  modelType.findById(id, cb);
+};
+
+//Read
+var getPlayer = partial(get, player.model);
+var getCharacter = partial(get, character.model);
+var getGame = partial(get, game.model);
+var getVideo = partial(get, video.model);
+var getCaster = partial(get, caster.model);
+var getEvent = partial(get, event.model);
+var getChannel = partial(get, channel.model);
+var getTeam = partial(get, team.model);
+var getFighter = partial(get, fighter.model);
+
+module.exports = {
+  createPlayer: createPlayer, 
+  createCharacter: createCharacter,
+  createGame: createGame,
+  createCaster: createCaster,
+  createEvent: createEvent,
+  createVideo: createVideo,
+  createChannel: createChannel,
+  createTeam: createTeam,
+  createFighter: createFighter,
+  getPlayer: getPlayer, 
+  getCharacter: getCharacter,
+  getGame: getGame,
+  getCaster: getCaster,
+  getEvent: getEvent,
+  getVideo: getVideo,
+  getChannel: getChannel,
+  getTeam: getTeam,
+  getFighter: getFighter,
 }
-
-var createCharacter = function(req,res){
-  character['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
-}
-
-var createGame = function(req,res){
-  game['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
-}
-
-var createVideo = function(req,res){
-  video['model'].create({url: req.body.url}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send('vid created');
-  });
-}
-
-var createCaster = function(req,res){
-  caster['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
-}
-
-var createEvent = function(req,res){
-  event['model'].create(
-    {
-      name: req.body.name,
-      date: req.body.date,
-      country: req.body.country
-    }, function(err, result){
-      if (err) {console.log(err)}
-      else res.status(200).send('event created');
-    }
-  );
-}
-
-var createChannel = function(req,res){
-  channel['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
-}
-
-var createTeam = function(req,res){
-  team['model'].create({name: req.body.name}, function(err, result){
-    if (err){console.log(err)}
-    else res.status(200).send();
-  });
-}
-
-var createFighter = function(req, res){
-  //player ID
-  player = req.body.player;
-  //character ID's in an []
-  characters = req.body.characters;
-  
-  fighter['model'].create(
-  { _player : player,
-    _characters : characters}, 
-    function(err, result){
-        if (err){console.log(err)}
-        else res.status(200).send('fighter created');
-  });
-  
-}
-
-module.exports = {'createPlayer': createPlayer, 
-                  'createCharacter': createCharacter,
-                  'createGame': createGame,
-                  'createCaster': createCaster,
-                  'createEvent': createEvent,
-                  'createVideo': createVideo,
-                  'createChannel': createChannel,
-                  'createTeam': createTeam
-                  }
