5e96e3a5a49d06757e5e0bc8977807ad989ff326
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-04 23:16:25 -0600

    added all creation routes for get and post to app.js

--- app.js
+++ app.js
@@ -22,25 +22,85 @@ var returnIndex = function (req, res) {
 app.get("/", returnIndex);
 app.get("/matches", returnIndex);
 
-app.post("/players", function(req, res){
-  res.send("creating");
+//PLAYER
+app.get("/create/player", function (req, res) {
+  res.render("createPlayer");
 });
-app.post("/characters", function(req, res){
-  res.send("creating");
-});
-app.post("/games", function(req, res){
-  res.send("creating");
-});
-app.post("/casters", function(req, res){
-  res.send("creating");
-});
-app.post("/videos", function(req, res){
-  res.send("creating");
-});
-app.post("/matches", function(req, res){
-  res.send("creating");
+app.post("/create/player", function(req, res){
+  res.send("creating player");
 });
 
+//CHARACTER
+app.get("/create/character", function (req, res) {
+  res.render("createPlayer");
+});
+app.post("/create/character", function(req, res){
+  res.send("creating character");
+});
+
+//GAME
+app.get("/create/game", function (req, res) {
+  res.render("createGame");
+});
+app.post("/create/game", function(req, res){
+  res.send("creating game");
+});
+
+//CASTER
+app.get("/create/caster", function (req, res) {
+  res.render("createCaster");
+});
+app.post("/create/caster", function(req, res){
+  res.send("creating caster");
+});
+
+//VIDEO
+app.get("/create/video", function (req, res) {
+  res.render("createVideo");
+});
+app.post("/create/video", function(req, res){
+  res.send("creating video");
+});
+
+//EVENT
+app.get("/create/event", function (req, res) {
+  res.render("createEvent");
+});
+app.post("/create/event", function (req, res) {
+  res.send("creating event");
+});
+
+//CHANNEL
+app.get("/create/channel", function (req, res) {
+  res.render("createEvent");
+});
+app.post("/create/channel", function (req, res) {
+  res.send("creating channel");
+});
+
+//TEAM
+app.get("/create/team", function (req, res) {
+  res.render("createEvent");
+});
+app.post("/create/team", function (req, res) {
+  res.send("creating team");
+});
+
+//PARTICIPANT
+app.get("/create/participant", function (req, res) {
+  res.render("createEvent");
+});
+app.post("/create/participant", function (req, res) {
+  res.send("creating participant");
+});
+
+//MATCH
+app.get("/create/match", function (req, res) {
+  res.render("createMatch");
+});
+app.post("/create/match", function(req, res){
+  res.send("creating match");
+});
 
 app.get('/matches/:id', function (req, res) {
   var id = Number(req.params.id)
