77d43031031886f949071e895f3eb66b8f6b94e2
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-06 13:45:47 -0600

    added seed data for casters and videos

---
+++ databaseScripts/seeds/videos.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {name: "Alioune vs Xian 1/3", url: "//www.youtube.com/embed/rY1gSpjJkOI"},
+  {name: "Alioune vs Xian 2/3", url: "//www.youtube.com/embed/wQPeIuhXPhQ"},
+  {name: "Alioune vs Xian 3/3", url: "//www.youtube.com/embed/CDx178qY5MU"},
+  {name: "Infiltration vs Tokido", url: "//www.youtube.com/embed/-Igjo5VXpDI"},
+  {name: "Mike Ross vs Xian", url: "//www.youtube.com/embed/2I3X2cO9InQ"},
+];
