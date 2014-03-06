ed3c58f7ecf9bf8a3989c5f6549a446b37814a1d
Peter Chen <pchen@4-south.com>
2014-03-06 15:19:22 -0600

    updated scripts

--- models/gameModel.js
+++ models/gameModel.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Games = new mongoose.Schema({
   name: String,
   nickname: String,
-  _characters: [{ type: mongoose.Schema.Types.ObjectId, ref: "Game" }]
+  _characters: [{ type: mongoose.Schema.Types.ObjectId, ref: "Character" }]
 });
 
 var Game = mongoose.model('Game', Games);
