0701f026ca382c66550f522cc2fb4a62d944078c
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-06 13:20:16 -0600

    added nickname to game model and form.  created game seed data

---
+++ databaseScripts/seeds/games.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {name: "Super Street Figher IV AE 2012", nickname: "SSF4 AE2012"},
+  {name: "Ultimate Marvel vs Capcom 3", nickname: "UMVC3"},
+  {name: "Super Smash Brothers Melee", nickname: "SBM"},
+  {name: "Super Smash Brothers Brawl", nickname: "SBB"},
+];
