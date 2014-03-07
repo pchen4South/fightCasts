8ffb6fcff57d5c9eff478cb209924a7be2950658
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-06 19:54:05 -0600

    moved helpers to viewshelpers ls

---
+++ views/helpers/gameIcon.js
@@ -0,0 +1,12 @@
+module.exports = function (game) {
+  switch (game) {
+    case "SF4":{
+      return "./images/SF4.png";
+      break;
+    }
+    case "SSBM":{
+      return "./images/SSBM.png";
+      break;
+    }
+  }
+}
