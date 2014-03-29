babeda6f0144ff23e15b36a87354b5e4eb759d7f
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-03-28 21:01:33 -0500

    removed match creation logic from reset reseed

--- databaseScripts/reset_reseed_db.js
+++ databaseScripts/reset_reseed_db.js
@@ -8,7 +8,6 @@ var async = require('async');
 
 var people = require('./seeds/people');
 var events = require('./seeds/events');
-var matches = require('./seeds/matches');
 var games = require('../models/gameCharacterData');
 
 var personModel = require('../models/personModel');
@@ -24,10 +23,6 @@ var eventsCreation = function(done){
   async.map(events, partial(create, eventModel.model), done);
 };
 
-var matchCreation = function(done){
-  async.map(matches, findModelsAndCreateMatches, done);
-}
-
 var find = function(modelType, query, done){
   modelType.model.find({name: query}, function(err, res){
     if (err) { 
@@ -39,60 +34,6 @@ var find = function(modelType, query, done){
     }
   });
 };
-
-var findPersonForCaster = function(casterName, cb){
-  personModel.model.find({name: casterName}, function(err,res){
-    if (err){
-      cb(err);
-    }
-    else
-    {
-     cb(err, res);
-    }
-  });
-}
-
-
-var findCasters = function(casters, cb){
-  async.map(casters, findPersonForCaster, cb);  
-};
-
-var findModelsAndCreateMatches  = function(match, done){
-  async.parallel([
-    //match.title -> title
-    async.apply(findCasters, match.casterNames),
-    async.apply(find, eventModel, match.eventName),
-  ], function(err, results){
-    var casterArray = results[2];
-    var event = results[4][0];
-    var casters = cleanNestedArray(casterArray);
-    
-    //results is an array of all the models
-    //results[0] is an array of [{playerJSON}]
-    matchModel.model.create({
-      title: match.title,
-      description: match.description,
-      game: match.game,
-      category: match.category
-      _casters: casters,
-      _event: event,
-    }, function(err,res){
-        if (err) done(err);
-        else done(null, res);      
-    });
-  });
-};
-
-var cleanNestedArray = function(arr){
-  var cleaned = [];
-   
-  for(var i = 0; i < arr.length; i++){
-    cleaned[i] = arr[i][0];
-  }
-  //console.log(cleaned);
-  return cleaned;
-}
-
 
 var create = function(modelType, data, cb){
   modelType.create(data, function(err, res){
@@ -107,18 +48,6 @@ var create = function(modelType, data, cb){
   });  
 }
 
-var featuredMatchCreation = function(cb){
-  matchModel.model.find({}, function(err, res){
-    console.log(res[3]);
-    featuredMatchModel.model.create({_match: res[0], game: res[0].game, category: res[0].category}, function(err,result){
-       featuredMatchModel.model.create({_match: res[3], game: res[3].game, category: res[3].category}, function(err,result){
-          console.log(res);
-          cb(null,res);
-      })
-    })
- })
-}
-
 var resetDb = function (mongoose) {
   mongoose.connect('mongodb://localhost:27017/fightCasts', function (err) {
     mongoose.connection.db.dropDatabase(function (err) {
@@ -126,8 +55,6 @@ var resetDb = function (mongoose) {
       async.series([
         peopleCreation,
         eventsCreation,
-        matchCreation,
-        featuredMatchCreation
       ], function(err, res){
         if (err) console.log (err);
         else mongoose.disconnect();
--- databaseScripts/seeds/matches.js
+++ databaseScripts/seeds/matches.js
@@ -1,68 +1,10 @@
-module.exports = [{
-  title: "Ryan Hart vs. Daigo",
-  videoNames: ["Daigo vs Ryan Hart"],
-  fighterOne: "Daigo Umehara",
-  fighterTwo: "Ryan Hart",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: ["Steve", "Pete"],
-  eventName: "ESGN Fight Night Season 1",
-  category: "pro",
-  featured: true,
-  approved: true,
-  description: "Nail-biter!  Fantastic grand finals."
-},{
-  title: "Infiltration vs. Daigo",
-  videoNames: "Infiltration vs Daigo",
-  fighterOne: "Daigo Umehara",
-  fighterTwo: "Infiltration",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: ["Steve", "Rohan"],
-  eventName: "ESGN Fight Night Season 3",
-  category: "pro",
-  description: "description placeholder"
-},{
-  title: "Pete vs Steve Casuals",
-  videoNames: "Alioune vs Xian 1/3",
-  fighterOne: "Pete",
-  fighterTwo: "Steve",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: ["Sid", "Rohan"],
-  eventName: "",
-  category: "scrub",
-  description: "description placeholder"
-},{
-  title: "OnlineWarrior vs Kappa",
-  videoNames: "Alioune vs Xian 2/3",
-  fighterOne: "Rohan", 
-  fighterTwo: "Gootecks",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: "",
-  eventName: "",
-  category: "community",
-  featured: true,
-  approved: true,
-  description: "description placeholder"
-}
-,{
-  title: "Steve vs Peter SSBM",
-  videoNames: "Pete vs Viperboy",
-  fighterOne: "Pete", 
-  fighterTwo: "Steve",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: ["Rohan"],
-  eventName: "",
-  category: "community",
-  description: "description placeholder"
-}
-,{
-  title: "Infiltration vs. Ryan Hart",
-  videoNames: "Infiltration vs Tokido",
-  fighterOne: "Ryan Hart", 
-  fighterTwo: "Infiltration",
-  game: "Super Street Fighter 4 Arcade Edition",
-  casterNames: ["Gootecks", "Mike Ross"],
-  eventName: "ESGN Fight Night Season 2",
-  category: "pro",
-  description: "description placeholder"
-}
-]
+module.exports = [
+  {
+    title: "Ryan Hart vs. Daigo",
+    game: "1",
+    casterNames: ["Steve", "Pete"],
+    eventName: "ESGN Fight Night Season 1",
+    category: "pro",
+    description: "Nail-biter!  Fantastic grand finals."
+  }
+];
