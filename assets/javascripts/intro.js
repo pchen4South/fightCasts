e0b81e46141dae04c61aa7cf7cfe5bd026d8c520
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-04-01 20:02:06 -0500

    added intro js and some basic settings to project for main home screen

--- assets/javascripts/intro.js
+++ assets/javascripts/intro.js
@@ -1,1 +1,49 @@
+var introButton = document.getElementById("intro");
+var tour = introJs();
 
+tour.setOptions({
+  showStepNumbers: false,
+  scrollToElement: false,
+  steps: [
+    {
+      element: "#featuredPro",
+      intro: "Top pro matches are featured here.  You can even view them directly in the tile.",
+      position: "right"
+    },
+    {
+      element: "#featuredCommunity",
+      intro: "Top community matches are featured here.  Again, the match may be viewed in the tile itself.",
+      position: "left"
+    },
+    {
+      element: "#proList",
+      intro: "The latest pro matches are always available in this list.",
+      position: "right"
+    },
+    {
+      element: "#communityList",
+      intro: "The latest community matches are always available in this list.",
+      position: "left"
+    },
+    {
+      element: "#focusedContent",
+      intro: "The currently focused match is found here.  Match details, comments, etc are available below.",
+      position: "bottom"
+    },
+    {
+      element: "#gameSelect",
+      intro: "Select your game of interest here.",
+      position: "right"
+    },
+    {
+      element: "#searchBar",
+      intro: "Typing in search terms will filter the lists and remove featured tiles.",
+      position: "bottom"
+    },
+  ] 
+});
+
+introButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  tour.start();
+});
