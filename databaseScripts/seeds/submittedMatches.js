1cf768b5f552ce430b0e8f749f6f1e8a32a92d44
Peter Chen <pchen@4-south.com>
2014-04-03 17:51:21 -0500

    added files

---
+++ databaseScripts/seeds/submittedMatches.js
@@ -0,0 +1,14 @@
+module.exports = [{"title":"ergea",
+  "game":{"id":"531fd67893fb15cc20338d57",
+          "name":"Super Street Fighter 4 Arcade Edition",
+          "nickname":"SF4AE"},
+  "fighterData":[ {"name":"gregre",
+                 "characters":[{
+                    "name":"C. Viper",
+                    "game":"SF4AE",
+                    "id":"531fd67893fb15cc20338d2f"}]
+              }],
+  "eventName":"gergera",
+  "casterNames":["gerg"],
+  "videoData":[{"url":"ergerg"}]
+}]
