0f31d17b56cbe12e874841d57e8a7744d3ecb757
Peter Chen <pchen@4-south.com>
2014-04-07 17:07:34 -0500

    search by previous week/month/year

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -82,4 +82,5 @@
     submitContact(email)
   });
 
+ 
 })(window);
--- routes/utils.js
+++ routes/utils.js
@@ -9,4 +9,32 @@ var createQuery = function (params) {
   };
 };
 
+var createHistoricalQuery = function(params){
+  var timeFrame = params.search;
+  var monQuery = {};
+  var now = new Date();
+  
+  switch(timeFrame){
+    case "previous week": 
+      monQuery = {"playedAt": 
+        {"$gte": new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7), 
+        "$lt": now}};
+      break;    
+    case "previous month": 
+      monQuery = {"playedAt": 
+        {"$gte": new Date(now.getFullYear(), now.getMonth(), now.getDate() - 30), 
+        "$lt": now}};
+      break;    
+      
+    case "previous year": 
+      monQuery = {"playedAt": 
+        {"$gte": new Date(now.getFullYear(), now.getMonth(), now.getDate() - 365), 
+        "$lt": now}};
+      break;
+  }
+  return monQuery;
+};
+
+
 module.exports.createQuery = createQuery;
+module.exports.createHistoricalQuery = createHistoricalQuery;
