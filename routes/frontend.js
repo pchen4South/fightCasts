238fbefe248047873a0fb2b1a3b916df58db2af2
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-05-01 17:20:58 -0500

    updated account signup and login pages to exist and have similar structure

--- routes/frontend.js
+++ routes/frontend.js
@@ -72,6 +72,14 @@ module.exports = function (app) {
     }
   });
 
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  app.get("/signup", function (req, res) {
+    res.render("signup");
+  });
+
   app.get("/", function (req, res) {
     res.redirect("/ssf4-ae2012/matches");
   });
